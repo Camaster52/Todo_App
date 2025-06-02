b7d782fe6b9c355b8e49a7298a0fb767bcd5b035
Gleb <filingleb06@gmail.com>
2025-06-02 09:22:35 +0200

    JWT fix + Header fix

--- client/src/components/layout/index/header.jsx
+++ client/src/components/layout/index/header.jsx
@@ -4,13 +4,17 @@ import { motion } from "framer-motion"
 const Header = () => {
 
     return(
-        <motion.header className="header">
-            <Menu className="header__menuSvg"/>
+        <motion.header className="header">\
+            <button className="header__svgButtonMenu">
+                <Menu className="header__svgButton-menu"/>
+            </button>
             <h1 className="header__title">Todo App</h1>
-            <svg className="header__exit"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 21H5a2 2 0 01-2-2V5a2 2 0 012-2h4M16 17l5-5-5-5M21 12H9" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="exit-path"/>
-                <path d="M13 12H9" stroke="#ffffff"strokeWidth="2"strokeLinecap="round"/>
-            </svg>
+            <button className="header__svgButtonExit">
+                <svg className="header__svgButton-exit"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9 21H5a2 2 0 01-2-2V5a2 2 0 012-2h4M16 17l5-5-5-5M21 12H9" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                    <path d="M13 12H9" stroke="#ffffff"strokeWidth="2"strokeLinecap="round"/>
+                </svg>
+            </button>
         </motion.header>
     )
 }
